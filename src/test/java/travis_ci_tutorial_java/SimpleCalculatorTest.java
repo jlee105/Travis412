1494cbfcbdcc0d0a45e75a9f8aeb8d07271aed59
Jacob Lee <jlee105@students.towson.edu>
2019-04-01 20:09:34 -0400

    added the the multiply and divide methods to the class and test files!

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,19 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	
+	@Test 
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator(); 
+		assertEquals(calc.multiply(5, 2), 10);
+	}
+	
+	@Test 
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(10, 2), 5);
+	}
+	
+	
+	
 }
